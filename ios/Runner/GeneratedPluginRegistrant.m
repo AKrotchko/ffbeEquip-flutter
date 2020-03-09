87902d808b5c10b72286c0d6b460696e5ba1b2ac
AKrotchko <47993343+AKrotchko@users.noreply.github.com>
2020-03-09 10:23:08 -0400

    Updated json data
    Included item data and unit passive data. Will need to make changes to how these are being handled in the future if the size is consistently getting larger.

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,7 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_keyboard_visibility/KeyboardVisibilityPlugin.h>)
 #import <flutter_keyboard_visibility/KeyboardVisibilityPlugin.h>
+#else
+@import flutter_keyboard_visibility;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
